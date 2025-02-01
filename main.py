631f3357202b821d4f84043f8701297b09e62b05
dixit2894 <113826012+dixit2894@users.noreply.github.com>
2025-02-01 13:02:03 +0530

    Update main.py
    API key remove

--- main.py
+++ main.py
@@ -87,7 +87,7 @@ def get_vectorstore(text_chunks):
     return vectorstore
 
 def get_conversion_chain(vectorstore):
-    chat = ChatGroq(temperature=0.7, groq_api_key="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", model_name="llama-3.3-70b-versatile")
+    chat = ChatGroq(temperature=0.7, groq_api_key="your_groq_api_key", model_name="llama-3.3-70b-versatile")
 
     memory = ConversationBufferMemory(
         memory_key='chat_history',
